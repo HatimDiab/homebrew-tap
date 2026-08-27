5b0a50f1f7d4872e7666ac68d28cfc5dc1ef044f
Hatim Diab <h@timdiab.com>
2026-08-27 19:01:58 +0200

    llcpp 0.5.0

--- Formula/llcpp.rb
+++ Formula/llcpp.rb
@@ -3,8 +3,8 @@ class Llcpp < Formula
 
   desc "Ollama-style CLI for llama.cpp: pull, run, search and serve GGUF models"
   homepage "https://github.com/HatimDiab/llcpp"
-  url "https://github.com/HatimDiab/llcpp/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "843aab4f3fa5380effc09ebc7b7e81c82b71c67952c3bd94ec3eeb36bcd83bda"
+  url "https://github.com/HatimDiab/llcpp/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "b3ac930f2b8df461d57454bd1d5fea22925694d9ba125630ae074b4f2ade2a1f"
   license "MIT"
 
   depends_on "llama.cpp"
